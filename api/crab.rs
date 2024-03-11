29c321218b7344f01f050e068449b675bad336e7
capJavert <ante@kickass.website>
2024-03-11 21:22:21 +0100

    feat: add rust support

---
+++ api/crab.rs
@@ -0,0 +1,17 @@
+use serde_json::json;
+use vercel_runtime::{run, Body, Error, Request, Response, StatusCode};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    run(handler).await
+}
+
+pub async fn handler(_req: Request) -> Result<Response<Body>, Error> {
+    Ok(Response::builder()
+        .status(StatusCode::OK)
+        .header("Content-Type", "application/json")
+        .body(
+            json!({ "message": "crab is the best!" }).to_string()
+            .into(),
+        )?)
+}
--- next.config.js
+++ next.config.js
@@ -1,8 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  rewrites: async () => {
+    const rewrites = {
+      afterFiles: [
+        // apply any of your existing rewrites here
+      ],
+      fallback: []
+    }
+
+    // dev only, this allows for local api calls to be proxied to
+    // api routes that use rust runtime
+    if (process.env.NODE_ENV === 'development') {
+      rewrites.fallback.push({
+        source: '/api/:path*',
+        destination: 'http://0.0.0.0:3001/api/:path*'
+      })
+    }
+
+    return rewrites
+  }
 }
 
 module.exports = nextConfig
